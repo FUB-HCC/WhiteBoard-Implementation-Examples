a27bdba649ed24446a46cb9eb4ec8c32bab6c418
adm_clmb <adm_clmb@wing.fritz.box>
2020-06-11 21:08:00 +0200

    minor changes in the formatting

--- EchoExample/src/tcp/Client.java
+++ EchoExample/src/tcp/Client.java
@@ -2,19 +2,34 @@ package tcp;
 
 import java.io.*;
 import java.net.*;
+
 public class Client {
-    public static void main (String args[]) throws IOException{ //args[0] host name and args[1] port
-        Socket socket = new Socket(args[0], Integer.parseInt(args[1])); // e.g. localhost 1234
-        PrintStream out = new PrintStream(socket.getOutputStream());
-        BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream())); //BufferedReader reads Keyboard Input
-        BufferedReader keyboard = new BufferedReader(new InputStreamReader(System.in));
-        while(true) { // ^D will close dialogue
-            String message = keyboard.readLine();
-            if(message==null) break;
-            out.println(message);
-            String answer = in.readLine();
-            System.out.println(answer);
-        }
-        in.close(); out.close(); socket.close();
-    }
+	
+	/**
+	 * @param args[0] host name, e.g. localhost and args[1] port, e.g. 1234 
+	 * @throws IOException
+	 */
+	public static void main(String args[]) throws IOException { 
+
+		Socket socket = new Socket(args[0], Integer.parseInt(args[1])); 
+
+		PrintStream out = new PrintStream(socket.getOutputStream());
+		
+		// BufferedReader reads Keyboard Input
+		BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream())); 
+		BufferedReader keyboard = new BufferedReader(new InputStreamReader(System.in));
+
+		while (true) { // ^D will close dialogue
+			String message = keyboard.readLine();
+			if (message == null)
+				break;
+			out.println(message);
+			
+			String answer = in.readLine();
+			System.out.println(answer);
+		}
+		in.close();
+		out.close();
+		socket.close();
+	}
 }
